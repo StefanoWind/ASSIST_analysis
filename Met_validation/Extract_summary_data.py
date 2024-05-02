894cb0ba2b9f204c6ea138c338d47374aac9186f
StefanoWind <stefano.letizia@nrel.gov>
2024-05-01 18:08:21 -0600

    added T_fnt, NEN, finalized

--- Met_validation/Extract_summary_data.py
+++ Met_validation/Extract_summary_data.py
@@ -2,11 +2,6 @@
 
 import os
 cd=os.path.dirname(__file__)
-import sys
-sys.path.append('C:/Users/SLETIZIA/OneDrive - NREL/Desktop/PostDoc/Custom_functions')
-sys.path.append('C:/Users/SLETIZIA/OneDrive - NREL/Desktop/PostDoc/Custom_functions/dap-py')
-import myFunctions as SL
-from doe_dap_dl import DAP
 import numpy as np
 import xarray as xr
 from matplotlib import pyplot as plt
@@ -15,6 +10,7 @@ import matplotlib
 import glob
 import pandas as pd
 from datetime import datetime
+import re
 
 warnings.filterwarnings('ignore')
 plt.close('all')
@@ -26,56 +22,51 @@ matplotlib.rcParams['font.size'] = 14
 #%% Inputs
 
 #dataset
-download=True
-username = 'sletizia'#ask DAP (dapteam@pnnl.gov)
-password = 'pass_DAP1506@'
-start_time='2023-05-08 00:00'
-end_time='2023-10-16 00:00'
-
 IDs=[10,11,12]
-channels={10:'awaken/sg.assist.z01.00',
-          11:'awaken/sb.assist.z01.00',
-          12:'awaken/sc1.assist.z01.00'}
+sources={10:'/srv/data/nfs/assist/Assist-10/raw',
+         11:'/srv/data/nfs/assist/Assist-11/raw',
+         12:'/srv/data/nfs/assist/Assist-12/raw'}
+
+start_date = '2022-10-01 00:00' # first date to process
+end_date =   '2023-11-01 00:00' # last date to process
 
 #%% Initialization
-time_range_dap = [datetime.strftime(datetime.strptime(start_time, '%Y-%m-%d %H:%M'), '%Y%m%d%H%M%S'),
-                  datetime.strftime(datetime.strptime(end_time, '%Y-%m-%d %H:%M'), '%Y%m%d%H%M%S')]
-
-a2e = DAP('a2e.energy.gov',confirm_downloads=False)
-a2e.setup_cert_auth(username=username, password=password)
 
 #%% Main
 for ID in IDs:
-    if download:
-        filter = {
-            'Dataset': channels[ID],
-            'date_time': {'between': time_range_dap},
-            'file_type': 'cdf',
-            'ext1':'assistsummary'
-        }
-        a2e.download_with_order(filter, path=os.path.join('data','assist-'+str(ID)), replace=False)
+    files_all = np.array(sorted(glob.glob(os.path.join(sources[ID],'*assistsummary*'))))
+    t_file=[]
+    for f in files_all:
+        match = re.search(r'\d{8}\.\d{6}', f)
+        t=datetime.strptime(match.group(0),'%Y%m%d.%H%M%S')
+        t_file=np.append(t_file,t)
+
+    sel_t=(t_file>=datetime.strptime(start_date,'%Y-%m-%d %H:%M'))*(t_file<datetime.strptime(end_date,'%Y-%m-%d %H:%M'))
+    files_selected=files_all[sel_t]  
     
-    files=glob.glob(os.path.join('data','assist-'+str(ID),'*assistsummary*'))
-    
-    if len(files)>0:
+    if len(files_selected)>0:
         time=np.array([],dtype='datetime64')
         T_675=[]
-        T_amb=[]
+        T_fnt=[]
         T_abb=[]
-        for f in files:
+        NEN=[]
+        for f in files_selected:
+            print('Processing '+f)
             Data=xr.open_dataset(f)
             Data=Data.sortby('time')
             
             time=np.append(time,Data['time'].values+Data['base_time'].values+np.datetime64('1970-01-01T00:00'))
             T_675=np.append(T_675,Data['mean_Tb_675_680'].values-273.15)
-            T_amb=np.append(T_amb,Data['calibrationAmbientTemp'].values)
+            T_fnt=np.append(T_fnt,Data['frontEndEnclosureTemp'].values)
             T_abb=np.append(T_abb,Data['ABBapexTemp'].values)
+            NEN=np.append(NEN,Data['LWskyNEN'].values)
             
         Output=pd.DataFrame()
-        Output['T_675']=T_675
-        Output['T_amb']=T_amb
-        Output['T_abb']=T_abb
         Output['Time']=time
+        Output['T_675']=T_675
+        Output['T_frontend']=T_fnt
+        Output['T_abb']=T_abb
+        Output['NEN']=NEN
         
-        Output.set_index('Time').to_csv('data/Summary_T_'+str(ID)+'.csv')
+        Output.set_index('Time').to_csv(os.path.join(cd,'data/Summary_T_'+str(ID)+'.csv'))
             
