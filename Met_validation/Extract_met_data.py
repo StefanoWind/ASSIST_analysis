f9e3fe9985ff8bd1e3015d80d75d09235583ac7e
StefanoWind <stefano.letizia@nrel.gov>
2024-05-07 17:48:42 -0600

    added WS, SWR, finalized

--- Met_validation/Extract_met_data.py
+++ Met_validation/Extract_met_data.py
@@ -12,8 +12,8 @@ warnings.filterwarnings('ignore')
 
 #%% Inputs
 sources={10:'/srv/data/nfs/sletizia/awaken/sg.met.z01.b0/*nc',
-          11:'/srv/data/nfs/sletizia/awaken/sb.met.z01.b0/*nc',
-          12:'/srv/data/nfs/sletizia/awaken/sc1.met.z01.b0/*nc'}
+         11:'/srv/data/nfs/sletizia/awaken/sb.met.z01.b0/*nc',
+         12:'/srv/data/nfs/sletizia/awaken/sc1.met.z01.b0/*nc'}
 
 #%% Initialization
 
@@ -21,7 +21,9 @@ sources={10:'/srv/data/nfs/sletizia/awaken/sg.met.z01.b0/*nc',
 for s in sources.keys():
     T=[]
     WS=[]
+    AWS=[]
     RH=[]
+    SWR=[]
     time=np.array([],dtype='datetime64')
     files=sorted(glob.glob(sources[s]))
     for f in files:
@@ -30,13 +32,17 @@ for s in sources.keys():
 
         time=np.append(time,Data['time'])
         T=np.append(T,Data['temperature'].where(Data['qc_temperature']==0))
-        WS=np.append(WS,Data['average_wind_speed'].where(Data['qc_average_wind_speed']==0))
+        WS=np.append(WS,Data['wind_speed'].where(Data['qc_wind_speed']==0))
+        AWS=np.append(AWS,Data['average_wind_speed'].where(Data['qc_average_wind_speed']==0))
         RH=np.append(RH,Data['relative_humidity'].where(Data['qc_relative_humidity']==0))
+        SWR=np.append(SWR,Data['shortwave_radiation'].where(Data['qc_shortwave_radiation']==0))
         
     Output=pd.DataFrame()
     Output['temperature']=T
-    Output['average_wind_speed']=WS
+    Output['wind_speed']=WS
+    Output['average_wind_speed']=AWS
     Output['relative_humidity']=RH
+    Output['shortwave_radiation']=RH
     Output['Time']=time
     Output=Output.set_index('Time')
-    Output.to_csv(os.path.join(cd,'data/Met_T_WS_RH_'+str(s)+'.csv'))
+    Output.to_csv(os.path.join(cd,'data/Met_data_'+str(s)+'.csv'))
