2ee925b2204201da9214f22581b125158fd68c0e
StefanoWind <stefano.letizia@nrel.gov>
2024-05-08 17:59:47 -0600

    streamlined, finalized

--- Met_validation/Daily_cycles.py
+++ Met_validation/Daily_cycles.py
@@ -13,8 +13,6 @@ from matplotlib import pyplot as plt
 import warnings
 import matplotlib
 import pandas as pd
-from scipy import stats
-
 warnings.filterwarnings('ignore')
 plt.close('all')
 
@@ -34,9 +32,6 @@ timezone=-6#[hours] difference local time - UTC
 
 #stats
 max_sigma_T=5#[K] maximum uncertainty
-M_BS=100#number of bootstrap launches
-p_value=0.05#p-value for uncertianty bounds
-perc_lim=[5,95]#limits for percentile filter
 
 # graphics
 site_names={10:'North',
@@ -60,29 +55,6 @@ def met_uncertainty(T,WS,shield_uncertainty):
         unc_T2=0
     
     return unc_T1+unc_T2
-
-def filt_mean(x,perc_lim=[5,95]):
-    x[x<np.nanpercentile(x,perc_lim[0])]=np.nan
-    x[x>np.nanpercentile(x,perc_lim[1])]=np.nan    
-    return np.nanmean(x)
-
-def filt_BS_mean(x,perc_lim,p_value,M_BS,min_N=10):
-    x[x<np.nanpercentile(x,perc_lim[0])]=np.nan
-    x[x>np.nanpercentile(x,perc_lim[1])]=np.nan
-    x=x[~np.isnan(x)]
-    
-    if len(x)>=min_N:
-        x_BS=bootstrap(x,M_BS)
-        mean=np.mean(x_BS,axis=1)
-        BS=np.nanpercentile(mean,p_value)
-    else:
-        BS=np.nan
-    return BS
-
-def bootstrap(x,M):
-    i=np.random.randint(0,len(x),size=(M,len(x)))
-    x_BS=x[i]
-    return x_BS
 
 #%% Initialization
 Data=pd.read_csv(os.path.join(cd,source))
@@ -110,82 +82,66 @@ Data_daily_avg=Data.rolling(window=int(np.timedelta64(1,'D')/dt)).mean()
 Data_det=Data-Data_daily_avg
 
 for ID in IDs:
-    Data['T_{ID}_unb_0.0m'.format(ID=ID)]=Data['T_{ID}_0.0m'.format(ID=ID)]-np.nanmean(Data['T_{ID}_0.0m'.format(ID=ID)]-Data['T_{ID}_met'.format(ID=ID)])
-    Data['T_{ID}_unb_10.0m'.format(ID=ID)]=Data['T_{ID}_10.0m'.format(ID=ID)]-np.nanmean(Data['T_{ID}_10.0m'.format(ID=ID)]-Data['T_{ID}_met'.format(ID=ID)])
+    Data['T_unb_{ID}_0.0m'.format(ID=ID)]=Data['T_{ID}_0.0m'.format(ID=ID)]-np.nanmean(Data['T_{ID}_0.0m'.format(ID=ID)]-Data['T_{ID}_met'.format(ID=ID)])
+    Data['T_unb_{ID}_10.0m'.format(ID=ID)]=Data['T_{ID}_10.0m'.format(ID=ID)]-np.nanmean(Data['T_{ID}_10.0m'.format(ID=ID)]-Data['T_{ID}_met'.format(ID=ID)])
     
 T_met_avg={}
 T_0m_avg={}
 T_10m_avg={}
-SWR_avg={}
-T_abb_avg={}
-
-T_met_low={}
-T_0m_low={}
-T_10m_low={}
-SWR_low={}
-T_abb_low={}
-
-T_met_top={}
-T_0m_top={}
-T_10m_top={}
-SWR_top={}
-T_abb_top={}
-
 T_det_met_avg={}
 T_det_0m_avg={}
 T_det_10m_avg={}
-
-T_det_met_low={}
-T_det_0m_low={}
-T_det_10m_low={}
-
-T_det_met_top={}
-T_det_0m_top={}
-T_det_10m_top={}
-
+SWR_avg={}
+T_abb_avg={}
 DT_0m_avg={}
 DT_10m_avg={}
 DT_unb_0m_avg={}
 DT_unb_10m_avg={}
 
+T_met_low={}
+T_0m_low={}
+T_10m_low={}
+T_det_met_low={}
+T_det_0m_low={}
+T_det_10m_low={}
+SWR_low={}
+T_abb_low={}
+DT_0m_low={}
+DT_10m_low={}
+DT_unb_0m_low={}
+DT_unb_10m_low={}
+
+T_met_top={}
+T_0m_top={}
+T_10m_top={}
+T_det_met_top={}
+T_det_0m_top={}
+T_det_10m_top={}
+SWR_top={}
+T_abb_top={}
+DT_0m_top={}
+DT_10m_top={}
+DT_unb_0m_top={}
+DT_unb_10m_top={}
 
 for ID in IDs:
-    T_met_avg[ID]=stats.binned_statistic(Data['hour'],Data['T_{ID}_met'.format(ID=ID)],    statistic=  lambda x:filt_mean(x,perc_lim),bins=np.arange(-0.5,24))[0]
-    T_0m_avg[ID]= stats.binned_statistic(Data['hour'],Data['T_{ID}_0.0m'.format(ID=ID)],   statistic= lambda x:filt_mean(x,perc_lim),bins=np.arange(-0.5,24))[0]
-    T_10m_avg[ID]=stats.binned_statistic(Data['hour'],Data['T_{ID}_10.0m'.format(ID=ID)],  statistic=lambda x:filt_mean(x,perc_lim),bins=np.arange(-0.5,24))[0]
-    SWR_avg[ID]=  stats.binned_statistic(Data['hour'],Data['SWR_{ID}_met'.format(ID=ID)],  statistic=lambda x:filt_mean(x,perc_lim),bins=np.arange(-0.5,24))[0]
-    T_abb_avg[ID]=stats.binned_statistic(Data['hour'],Data['T_abb_{ID}_sum'.format(ID=ID)],statistic=lambda x:filt_mean(x,perc_lim),bins=np.arange(-0.5,24))[0]
+    T_met_avg[ID],T_met_low[ID],T_met_top[ID]=utl.bins_unc(Data['hour'],Data['T_{ID}_met'.format(ID=ID)],bins=np.arange(-0.5,24))
+    T_0m_avg[ID],T_0m_low[ID],T_0m_top[ID]=utl.bins_unc(Data['hour'],Data['T_{ID}_0.0m'.format(ID=ID)],bins=np.arange(-0.5,24))
+    T_10m_avg[ID],T_10m_low[ID],T_10m_top[ID]=utl.bins_unc(Data['hour'],Data['T_{ID}_10.0m'.format(ID=ID)],bins=np.arange(-0.5,24))
     
-    T_met_low[ID]=stats.binned_statistic(Data['hour'],Data['T_{ID}_met'.format(ID=ID)],    lambda x:filt_BS_mean(x,perc_lim,p_value/2*100,M_BS),bins=np.arange(-0.5,24))[0]
-    T_0m_low[ID]= stats.binned_statistic(Data['hour'],Data['T_{ID}_0.0m'.format(ID=ID)],   lambda x:filt_BS_mean(x,perc_lim,p_value/2*100,M_BS),bins=np.arange(-0.5,24))[0]
-    T_10m_low[ID]=stats.binned_statistic(Data['hour'],Data['T_{ID}_10.0m'.format(ID=ID)],  lambda x:filt_BS_mean(x,perc_lim,p_value/2*100,M_BS),bins=np.arange(-0.5,24))[0]
-    SWR_low[ID]=  stats.binned_statistic(Data['hour'],Data['SWR_{ID}_met'.format(ID=ID)],  lambda x:filt_BS_mean(x,perc_lim,p_value/2*100,M_BS),bins=np.arange(-0.5,24))[0]
-    T_abb_low[ID]=stats.binned_statistic(Data['hour'],Data['T_abb_{ID}_sum'.format(ID=ID)],lambda x:filt_BS_mean(x,perc_lim,p_value/2*100,M_BS),bins=np.arange(-0.5,24))[0]
-     
-    T_met_top[ID]=stats.binned_statistic(Data['hour'],Data['T_{ID}_met'.format(ID=ID)],    lambda x:filt_BS_mean(x,perc_lim,(1-p_value/2)*100,M_BS),bins=np.arange(-0.5,24))[0]
-    T_0m_top[ID]= stats.binned_statistic(Data['hour'], Data['T_{ID}_0.0m'.format(ID=ID)],  lambda x:filt_BS_mean(x,perc_lim,(1-p_value/2)*100,M_BS),bins=np.arange(-0.5,24))[0]
-    T_10m_top[ID]=stats.binned_statistic(Data['hour'],Data['T_{ID}_10.0m'.format(ID=ID)],  lambda x:filt_BS_mean(x,perc_lim,(1-p_value/2)*100,M_BS),bins=np.arange(-0.5,24))[0]
-    SWR_top[ID]=  stats.binned_statistic(Data['hour'],Data['SWR_{ID}_met'.format(ID=ID)],  lambda x:filt_BS_mean(x,perc_lim,(1-p_value/2)*100,M_BS),bins=np.arange(-0.5,24))[0]
-    T_abb_top[ID]=stats.binned_statistic(Data['hour'],Data['T_abb_{ID}_sum'.format(ID=ID)],lambda x:filt_BS_mean(x,perc_lim,(1-p_value/2)*100,M_BS),bins=np.arange(-0.5,24))[0]
+    T_det_met_avg[ID],T_det_met_low[ID],T_det_met_top[ID]=utl.bins_unc(Data['hour'],Data_det['T_{ID}_met'.format(ID=ID)],bins=np.arange(-0.5,24))
+    T_det_0m_avg[ID],T_det_0m_low[ID],T_det_0m_top[ID]=utl.bins_unc(Data['hour'],Data_det['T_{ID}_0.0m'.format(ID=ID)],bins=np.arange(-0.5,24))
+    T_det_10m_avg[ID],T_det_10m_low[ID],T_det_10m_top[ID]=utl.bins_unc(Data['hour'],Data_det['T_{ID}_10.0m'.format(ID=ID)],bins=np.arange(-0.5,24))
     
-    T_det_met_avg[ID]=stats.binned_statistic(Data['hour'],Data_det['T_{ID}_met'.format(ID=ID)],  statistic=lambda x:filt_mean(x,perc_lim),bins=np.arange(-0.5,24))[0]
-    T_det_0m_avg[ID]= stats.binned_statistic(Data['hour'],Data_det['T_{ID}_0.0m'.format(ID=ID)], statistic=lambda x:filt_mean(x,perc_lim),bins=np.arange(-0.5,24))[0]
-    T_det_10m_avg[ID]=stats.binned_statistic(Data['hour'],Data_det['T_{ID}_10.0m'.format(ID=ID)],statistic=lambda x:filt_mean(x,perc_lim),bins=np.arange(-0.5,24))[0]
+    SWR_avg[ID],SWR_low[ID],SWR_top[ID]=utl.bins_unc(Data['hour'],Data['SWR_{ID}_met'.format(ID=ID)],bins=np.arange(-0.5,24))
+    T_abb_avg[ID],T_abb_low[ID],T_abb_top[ID]=utl.bins_unc(Data['hour'],Data['T_abb_{ID}_sum'.format(ID=ID)],bins=np.arange(-0.5,24))
     
-    T_det_met_low[ID]=stats.binned_statistic(Data['hour'],Data_det['T_{ID}_met'.format(ID=ID)],  lambda x:filt_BS_mean(x,perc_lim,p_value/2*100,M_BS),bins=np.arange(-0.5,24))[0]
-    T_det_0m_low[ID]= stats.binned_statistic(Data['hour'],Data_det['T_{ID}_0.0m'.format(ID=ID)], lambda x:filt_BS_mean(x,perc_lim,p_value/2*100,M_BS),bins=np.arange(-0.5,24))[0]
-    T_det_10m_low[ID]=stats.binned_statistic(Data['hour'],Data_det['T_{ID}_10.0m'.format(ID=ID)],lambda x:filt_BS_mean(x,perc_lim,p_value/2*100,M_BS),bins=np.arange(-0.5,24))[0]
-     
-    T_det_met_top[ID]=stats.binned_statistic(Data['hour'],Data_det['T_{ID}_met'.format(ID=ID)],  lambda x:filt_BS_mean(x,perc_lim,(1-p_value/2)*100,M_BS),bins=np.arange(-0.5,24))[0]
-    T_det_0m_top[ID]= stats.binned_statistic(Data['hour'],Data_det['T_{ID}_0.0m'.format(ID=ID)], lambda x:filt_BS_mean(x,perc_lim,(1-p_value/2)*100,M_BS),bins=np.arange(-0.5,24))[0]
-    T_det_10m_top[ID]=stats.binned_statistic(Data['hour'],Data_det['T_{ID}_10.0m'.format(ID=ID)],lambda x:filt_BS_mean(x,perc_lim,(1-p_value/2)*100,M_BS),bins=np.arange(-0.5,24))[0]
+    DT_0m_avg[ID],DT_0m_low[ID],DT_0m_top[ID]=utl.bins_unc(Data['hour'],Data['T_{ID}_0.0m'.format(ID=ID)]-Data['T_{ID}_met'.format(ID=ID)],bins=np.arange(-0.5,24))
+    DT_10m_avg[ID],DT_10m_low[ID],DT_10m_top[ID]=utl.bins_unc(Data['hour'],Data['T_{ID}_10.0m'.format(ID=ID)]-Data['T_{ID}_met'.format(ID=ID)],bins=np.arange(-0.5,24))
     
-    DT_0m_avg[ID]= stats.binned_statistic(Data['hour'],Data['T_{ID}_0.0m'.format(ID=ID)]- Data['T_{ID}_met'.format(ID=ID)],statistic=lambda x:filt_mean(x,perc_lim),bins=np.arange(-0.5,24))[0]
-    DT_10m_avg[ID]=stats.binned_statistic(Data['hour'],Data['T_{ID}_10.0m'.format(ID=ID)]-Data['T_{ID}_met'.format(ID=ID)],statistic=lambda x:filt_mean(x,perc_lim),bins=np.arange(-0.5,24))[0]
+    DT_unb_0m_avg[ID],DT_unb_0m_low[ID],DT_unb_0m_top[ID]=utl.bins_unc(Data['hour'],Data['T_unb_{ID}_0.0m'.format(ID=ID)]-Data['T_{ID}_met'.format(ID=ID)],bins=np.arange(-0.5,24))
+    DT_unb_10m_avg[ID],DT_unb_10m_low[ID],DT_unb_10m_top[ID]=utl.bins_unc(Data['hour'],Data['T_unb_{ID}_10.0m'.format(ID=ID)]-Data['T_{ID}_met'.format(ID=ID)],bins=np.arange(-0.5,24))
     
-    DT_unb_0m_avg[ID]= stats.binned_statistic(Data['hour'],Data['T_{ID}_unb_0.0m'.format(ID=ID)]-Data['T_{ID}_met'.format(ID=ID)],statistic=lambda x:filt_mean(x,perc_lim),bins=np.arange(-0.5,24))[0]
-    DT_unb_10m_avg[ID]=stats.binned_statistic(Data['hour'],Data['T_{ID}_unb_10.0m'.format(ID=ID)]-Data['T_{ID}_met'.format(ID=ID)],statistic=lambda x:filt_mean(x,perc_lim),bins=np.arange(-0.5,24))[0]        
- 
 #%% Plots
 plt.close('all')
 #absolute temperature 
@@ -194,7 +150,7 @@ for ID in IDs:
     ax=plt.subplot(1,len(IDs),np.where(ID==np.array(IDs))[0][0]+1)
     plt.plot(np.arange(24),T_met_avg[ID],'k',label='Met at 2 m')
     ax.fill_between(np.arange(24),T_met_low[ID],T_met_top[ID],color='k', alpha=0.25)
-    plt.ylabel(r'$\Delta T$ [$^\circ$]')
+    plt.ylabel(r'$T$ [$^\circ$C]')
     
     plt.plot(np.arange(24),T_0m_avg[ID],'b',label='TROPoe at 0 m')
     ax.fill_between(np.arange(24),T_0m_low[ID],T_0m_top[ID],color='b', alpha=0.25)
@@ -215,7 +171,7 @@ for ID in IDs:
     ax=plt.subplot(1,len(IDs),np.where(ID==np.array(IDs))[0][0]+1)
     plt.plot(np.arange(24),T_det_met_avg[ID],'k',label='Met at 2 m')
     ax.fill_between(np.arange(24),T_det_met_low[ID],T_det_met_top[ID],color='k', alpha=0.25)
-    plt.ylabel(r'$\Delta T$ [$^\circ$]')
+    plt.ylabel(r'$\tilde{T}$ [$^\circ$C]')
     
     plt.plot(np.arange(24),T_det_0m_avg[ID],'b',label='TROPoe at 0 m')
     ax.fill_between(np.arange(24),T_det_0m_low[ID],T_det_0m_top[ID],color='b', alpha=0.25)
@@ -230,61 +186,57 @@ for ID in IDs:
 utl.remove_labels(fig)
 plt.legend()
 
-#solar radiation
-fig=plt.figure(figsize=(18,5))
+#solar radiation, ABB
+fig=plt.figure(figsize=(14,10))
 for ID in IDs:
-    ax1=plt.subplot(1,len(IDs),np.where(ID==np.array(IDs))[0][0]+1)
+    ax1=plt.subplot(3,len(IDs),np.where(ID==np.array(IDs))[0][0]+1)
     plt.plot(np.arange(24),T_met_avg[ID],'k',label='Met at 2 m')
     ax1.fill_between(np.arange(24),T_met_low[ID],T_met_top[ID],color='k', alpha=0.25)
     plt.plot(np.arange(24),T_abb_avg[ID],'g',label='ABB')
     ax1.fill_between(np.arange(24),T_abb_low[ID],T_abb_top[ID],color='g', alpha=0.25)
-    plt.ylabel(r'T [C$^\circ$]')
+    plt.ylabel(r'T [$^\circ$C]')
     plt.ylim([0,50])
     plt.yticks(np.arange(0,51,10))
+    plt.xticks([0,6,12,18],labels=['00','06','12','18'])
+    plt.grid()
+    plt.title(site_names[ID])
+    if ID==IDs[-1]:
+        plt.legend()
     
-    ax2 = ax1.twinx()
-    plt.plot(np.arange(24),SWR_avg[ID],'y')
-    ax2.fill_between(np.arange(24),SWR_low[ID],SWR_top[ID],color='y', alpha=0.25)
+    ax2=plt.subplot(3,len(IDs),np.where(ID==np.array(IDs))[0][0]+4)
+    plt.plot(np.arange(24),SWR_avg[ID],'k')
+    ax2.fill_between(np.arange(24),SWR_low[ID],SWR_top[ID],color='k', alpha=0.25)
     plt.ylabel(r'SW radiation [m $^{-2}$]')
     plt.ylim([0,1000])
     plt.yticks(np.arange(0,1001,200))
     plt.grid()
-    plt.title(site_names[ID])
+    
     plt.xticks([0,6,12,18],labels=['00','06','12','18'])
+    
+    ax2=plt.subplot(3,len(IDs),np.where(ID==np.array(IDs))[0][0]+7)
+    plt.plot(np.arange(24),DT_0m_avg[ID],'k',label='Raw')
+    ax2.fill_between(np.arange(24),DT_0m_low[ID],DT_0m_top[ID],color='k', alpha=0.25)
+    plt.plot(np.arange(24),DT_unb_0m_avg[ID],'--k',label='Unbiased')
+    ax2.fill_between(np.arange(24),DT_unb_0m_low[ID],DT_unb_0m_top[ID],color='k', alpha=0.25)
+    plt.ylabel(r'$\Delta T$ (TROPoe 0 m - met 2 m) [$^\circ$C]')
+    plt.ylim([-1,1])
+    plt.yticks(np.arange(-1,1.01,0.5))
+    plt.grid()
+    plt.xticks([0,6,12,18],labels=['00','06','12','18'])
+    if ID==IDs[-1]:
+        plt.legend()
+    
     plt.xlabel('Hour')
 utl.remove_labels(fig)
-
-
-#profiles (biased)
-colors = plt.cm.coolwarm(np.linspace(0, 1, 24))
-fig=plt.figure(figsize=(18,9))
-for ID in IDs:
-    ax1=plt.subplot(len(IDs),1,np.where(ID==np.array(IDs))[0][0]+1)
-    for h in np.arange(24):
-        plt.plot(np.array([0,DT_0m_avg[ID][h],DT_10m_avg[ID][h]])+h,[2,3,10],'.-',color=plt.cm.coolwarm((T_met_avg[ID][h]-20)/12),markersize=12)
-    ax1.set_ylim([2,10])
-    ax1.set_xticks(np.arange(24))
-    ax1.set_yticks(np.arange(2,11,2))
-    ax1.grid(visible=True)
-    ax1.set_ylabel(r'$z$ [m.a.g.l.]')
-
-    ax2 = ax1.twinx()
-    ax2.fill_between(np.arange(24),DT_0m_avg[ID],np.arange(24)*0,color='k',alpha=0.25)
-    ax2.set_ylabel(r'$\Delta T$ (TROPoe 0 m - met 2 m) [$^\circ$]')
-    ax2.set_ylim([-0.5,0.5])
-    ax2.set_yticks(np.arange(-0.5,0.6,0.25))
-    plt.title(site_names[ID])
-ax1.set_xlabel('Hour')
-plt.tight_layout()
 
 #profiles (unbiased)
 plt.figure(figsize=(18,9))
 for ID in IDs:
     ax1=plt.subplot(len(IDs),1,np.where(ID==np.array(IDs))[0][0]+1)
     for h in np.arange(24):
-        plt.plot(np.array([0,DT_unb_0m_avg[ID][h],DT_unb_10m_avg[ID][h]])+h,[2,3,10],'.-',color=plt.cm.coolwarm((T_met_avg[ID][h]-20)/12),markersize=12)
+        plt.plot(np.array([0,DT_unb_0m_avg[ID][h],DT_unb_10m_avg[ID][h]])+h,[2,0,10],'.-',color=plt.cm.coolwarm((T_met_avg[ID][h]-20)/12),markersize=12)
         
-    ax1.set_ylim([2,10])
+    ax1.set_ylim([0,10])
     ax1.set_xticks(np.arange(24))
     ax1.set_yticks(np.arange(2,11,2))
     ax1.grid(visible=True)
@@ -292,7 +244,7 @@ for ID in IDs:
     
     ax2 = ax1.twinx()
     ax2.fill_between(np.arange(24),DT_unb_0m_avg[ID],np.arange(24)*0,color='k',alpha=0.25)
-    ax2.set_ylabel(r'$\Delta T$ (TROPoe 0 m - met 2 m) [$^\circ$]')
+    ax2.set_ylabel(r'$\Delta T$ (TROPoe 0 m - met 2 m) [$^\circ$C]')
     ax2.set_ylim([-0.4,0.4])
     ax2.set_yticks(np.arange(-0.4,0.5,0.2))
     plt.title(site_names[ID])
