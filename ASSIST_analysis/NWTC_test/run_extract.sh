6395b2c8ccd530c333c77fdb5238a4758cc45e70
StefanoWind <stefano.letizia@nrel.gov>
2025-01-15 17:31:30 -0700

    fixed config, removed .sh

--- ASSIST_analysis/NWTC_test/run_extract.sh
+++
@@ -1,3 +0,0 @@
-/home/sletizia/.conda/envs/awaken_sl/bin/python extract_spectra.py
-/home/sletizia/.conda/envs/awaken_sl/bin/python extract_cbh.py
-/home/sletizia/.conda/envs/awaken_sl/bin/python add_cloud_info.py
