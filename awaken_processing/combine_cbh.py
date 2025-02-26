c0c41383ed26150888053dcbe932e732766704c3
Stefano Letizia <sletizia@kl2.head.cm.kestrel.hpc.nrel.gov>
2025-02-25 17:57:42 -0700

    fixed combine_cbh for server

--- awaken_processing/combine_cbh.py
+++ awaken_processing/combine_cbh.py
@@ -22,13 +22,13 @@ plt.close('all')
 #%% Inputs
 
 #dataset
-sources={'A1':os.path.join(cd,'data/awaken/sa1.ceil.z01.cbh/*{datestr}*nc'),
-         'H':os.path.join(cd,'data/awaken/sgpceilS6.cbh/*{datestr}*nc')}
+sources={'A1':'/scratch/sletizia/data/awaken/sa1.ceil.z01.cbh/*{datestr}*nc',
+         'H':'/scratch/sletizia/data/awaken/sgpceilS6.cbh/*{datestr}*nc'}
 
-source_lyt=os.path.join(cd,'data','20250225_AWAKEN_layout.nc')
+path_save='/scratch/sletizia/data/awaken'
 
-sdate='20230601'
-edate='20230607'
+sdate='20221001'
+edate='20231031'
 
 #time interpolation
 dtime=60#[s]
@@ -42,7 +42,7 @@ dates=np.arange(np.datetime64(f"{sdate[:4]}-{sdate[4:6]}-{sdate[6:]}T00:00:00"),
                 np.datetime64(f"{edate[:4]}-{edate[4:6]}-{edate[6:]}T00:00:00")+np.timedelta64(1,'s'),np.timedelta64(1,'D'))
 time_offset=np.arange(0,3600*24+1,dtime)
 
-dir_save=os.path.join(cd,'data/awaken/','s'+'.s'.join(list(sources.keys())).lower()+'.ceil.z01.cbh')
+dir_save=os.path.join(path_save,'s'+'.s'.join(list(sources.keys())).lower()+'.ceil.z01.cbh')
 os.makedirs(dir_save,exist_ok=True)
 
 #%% Main
@@ -55,6 +55,7 @@ for d in dates:
     for s in sources:
         files=glob.glob(sources[s].format(datestr=str(d)[:10].replace('-','')))
         
+        basetime=0
         if len(files)==1:
             
             #load data
@@ -72,43 +73,44 @@ for d in dates:
             plt.plot(time_np,cbh_all[:,i_s],'-',color=colors[s],alpha=1,label=s)
         i_s+=1
     
-    #stats
-    cbh_sum=np.nanmean(cbh_all,axis=1)
-    cbh_sel=cbh_all[~np.isnan(np.sum(cbh_all,axis=1)),:]
-    rho=np.corrcoef(cbh_sel[:,0],cbh_sel[:,1])[0,1]
-    
-    #Output
-    Output=xr.Dataset()
-    Output['first_cbh']=xr.DataArray(data=np.int32(np.nan_to_num(cbh_sum,nan=-9999)),
-                                     coords={'time':np.int32(time_offset)},
-                                     attrs={'description':f'First cloud base height combining sites {list(sources.keys())}','units':'m'})
-    i_s=0
-    for s in sources:
-        Output[f'first_cbh_{s}']=xr.DataArray(data=np.int32(np.nan_to_num(cbh_all[:,i_s],nan=-9999)),
-                                         coords={'time':np.int32(time_offset)},
-                                         attrs={'description':f'First cloud base height from site {s}','units':'m'})
-        i_s+=1
+    if basetime>0:
+        #stats
+        cbh_sum=np.nanmean(cbh_all,axis=1)
+        cbh_sel=cbh_all[~np.isnan(np.sum(cbh_all,axis=1)),:]
+        rho=np.corrcoef(cbh_sel[:,0],cbh_sel[:,1])[0,1]
+        
+        #Output
+        Output=xr.Dataset()
+        Output['first_cbh']=xr.DataArray(data=np.int32(np.nan_to_num(cbh_sum,nan=-9999)),
+                                        coords={'time':np.int32(time_offset)},
+                                        attrs={'description':f'First cloud base height combining sites {list(sources.keys())}','units':'m'})
+        i_s=0
+        for s in sources:
+            Output[f'first_cbh_{s}']=xr.DataArray(data=np.int32(np.nan_to_num(cbh_all[:,i_s],nan=-9999)),
+                                            coords={'time':np.int32(time_offset)},
+                                            attrs={'description':f'First cloud base height from site {s}','units':'m'})
+            i_s+=1
 
-    Output['base_time']=np.int64(basetime)
-    Output.attrs['comment']='created on '+datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')+' by stefano.letizia@nrel.gov'
-    
-    name_save=os.path.basename(dir_save)+'.'+datetime.utcfromtimestamp(basetime).strftime('%Y%m%d.%H%M%S')+'.nc'
-    Output.to_netcdf(os.path.join(dir_save,name_save))
-    
-    #Plots
-    plt.plot(time_np,cbh_sum,'-',color='r',alpha=1,label='Summary')
-    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%H%M'))
-    plt.title('CBH on '+str(d)[:10])
-    plt.xlim([d,d+np.timedelta64(1, 'D')])
-    plt.ylim([0,12000])
-    plt.xlabel('Time (UTC)')
-    plt.ylabel(r'$z$ [m]')
-    plt.text(time_np[10],11500,r'$\rho='+str(np.round(rho,2))+'$',bbox={'facecolor':'w','alpha':0.5,'edgecolor':'k'})
-    plt.legend(loc='upper right')
-    plt.grid()
-    
-    plt.savefig(os.path.join(dir_save,name_save.replace('nc','png')))
-    plt.close()
-    
-    
+        Output['base_time']=np.int64(basetime)
+        Output.attrs['comment']='created on '+datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')+' by stefano.letizia@nrel.gov'
+        
+        name_save=os.path.basename(dir_save)+'.'+datetime.utcfromtimestamp(basetime).strftime('%Y%m%d.%H%M%S')+'.nc'
+        Output.to_netcdf(os.path.join(dir_save,name_save))
+        
+        #Plots
+        plt.plot(time_np,cbh_sum,'-',color='r',alpha=1,label='Summary')
+        plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%H%M'))
+        plt.title('CBH on '+str(d)[:10])
+        plt.xlim([d,d+np.timedelta64(1, 'D')])
+        plt.ylim([0,12000])
+        plt.xlabel('Time (UTC)')
+        plt.ylabel(r'$z$ [m]')
+        plt.text(time_np[10],11500,r'$\rho='+str(np.round(rho,2))+'$',bbox={'facecolor':'w','alpha':0.5,'edgecolor':'k'})
+        plt.legend(loc='upper right')
+        plt.grid()
+        
+        plt.savefig(os.path.join(dir_save,name_save.replace('nc','png')))
+        plt.close()
+        
+        
 
