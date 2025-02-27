8c2140ca8dabfa3657ca7adf387f2809ddb47a19
StefanoWind <stefano.letizia@nrel.gov>
2025-02-26 18:55:21 -0700

    excluding flat CBH\

--- awaken_processing/combine_cbh.py
+++ awaken_processing/combine_cbh.py
@@ -5,6 +5,7 @@ Combine CBH from different locations at AWAKEN
 
 import os
 cd=os.path.dirname(__file__)
+import sys
 import warnings
 from matplotlib import pyplot as plt
 import numpy as np
@@ -21,23 +22,29 @@ plt.close('all')
 
 #%% Inputs
 
-#dataset
-sources={'A1':'/scratch/sletizia/data/awaken/sa1.ceil.z01.cbh/*{datestr}*nc',
-          'H':'/scratch/sletizia/data/awaken/sgpceilS6.cbh/*{datestr}*nc'}
-
-path_save='/scratch/sletizia/data/awaken'
-
-# sources={'A1':os.path.join(cd,'data/awaken/sa1.ceil.z01.cbh/*{datestr}*nc'),
-#          'H': os.path.join(cd,'data/awaken/sgpceilS6.cbh/*{datestr}*nc')}
-
-# path_save=os.path.join(cd,'data/awaken')
-
-sdate='20221001'
-edate='20231031'
+if len(sys.argv)==1:
+    path_save=os.path.join(cd,'data/awaken')
+    source1='A1'
+    source2='H'
+    sdate='20230801'
+    edate='20231031'
+else:
+    path_save=sys.argv[1]
+    source1=sys.argv[2]
+    source2=sys.argv[3]
+    sdate=sys.argv[4]
+    edate=sys.argv[5]
+    
+#archive of sources
+sources={'A1':os.path.join(path_save,'sa1.ceil.z01.cbh/*{datestr}*nc'),
+          'H':os.path.join(path_save,'sgpceilS6.cbh/*{datestr}*nc')}
 
 #time interpolation
-dtime=60#[s]
-max_time_diff=30#[s]
+dtime=60#[s] time step
+max_time_diff=30#[s] maximum time difference
+
+#stats
+max_flat=0.2#ratio of points with 0 time derivative
 
 #graphics
 colors={'A1':'b','H':'g','E37':'m'}
@@ -50,19 +57,21 @@ time_offset=np.arange(0,3600*24+1,dtime)
 dir_save=os.path.join(path_save,'s'+'.s'.join(list(sources.keys())).lower()+'.ceil.z01.cbh')
 os.makedirs(dir_save,exist_ok=True)
 
+sources_sel=[source1,source2]
+
 #%% Main
 for d in dates:
     cbh_all=np.zeros((len(time_offset),len(sources)))+np.nan
     data={}
     i_s=0
     time_np=d+np.timedelta64(1,'s')*time_offset
-    plt.figure(figsize=(18,8))
-    for s in sources:
+    
+    for s in sources_sel:
         files=glob.glob(sources[s].format(datestr=str(d)[:10].replace('-','')))
         
-        found=False
+        found=0
         if len(files)==1:
-            found=True
+            found+=1
             
             #load data
             data=xr.open_dataset(files[0])
@@ -72,19 +81,28 @@ for d in dates:
                 data = data.groupby('time').mean()
     
             basetime=data.base_time.values
-            data=data.where(data['first_cbh']>0)
             
+            #qc
+            data=data.where(data['first_cbh']>0)
+            data['d_first_cbh_dtime']=np.abs(data.first_cbh.differentiate("time"))
+            if np.sum(data.d_first_cbh_dtime==0)/len(data.time)>max_flat:
+                print(f'{os.path.basename(files[0])} has too many flat points, skipped',flush=True)
+                found-=1
+                continue
+        
             #interpolate in time
             cbh_int=data.first_cbh.interp(time=time_offset)
             time_diff=data.time.interp(time=time_offset,method='nearest')-time_offset
             cbh_all[:,i_s]=cbh_int.where(np.abs(time_diff)<=max_time_diff)
     
             #plots
+            if plt.get_fignums()==[]:
+                plt.figure(figsize=(18,8))
             plt.plot(np.datetime64('1970-01-01T00:00:00')+np.timedelta64(1,'s')*(basetime+data.time),data.first_cbh,'.',color=colors[s],alpha=0.1)
             plt.plot(time_np,cbh_all[:,i_s],'-',color=colors[s],alpha=1,label=s)
         i_s+=1
     
-    if found:
+    if found>0:
         #stats
         cbh_sum=np.nanmean(cbh_all,axis=1)
         cbh_sel=cbh_all[~np.isnan(np.sum(cbh_all,axis=1)),:]
@@ -96,7 +114,7 @@ for d in dates:
                                         coords={'time':np.int32(time_offset)},
                                         attrs={'description':f'First cloud base height combining sites {list(sources.keys())}','units':'m'})
         i_s=0
-        for s in sources:
+        for s in sources_sel:
             Output[f'first_cbh_{s}']=xr.DataArray(data=np.int32(np.nan_to_num(cbh_all[:,i_s],nan=-9999)),
                                             coords={'time':np.int32(time_offset)},
                                             attrs={'description':f'First cloud base height from site {s}','units':'m'})
@@ -107,6 +125,7 @@ for d in dates:
         
         name_save=os.path.basename(dir_save)+'.'+datetime.utcfromtimestamp(basetime).strftime('%Y%m%d.%H%M%S')+'.nc'
         Output.to_netcdf(os.path.join(dir_save,name_save))
+        print(f'{name_save} created',flush=True)
         
         #Plots
         plt.plot(time_np,cbh_sum,'-',color='r',alpha=1,label='Summary')
