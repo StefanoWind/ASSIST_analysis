1e6c669b982223c293cf9200133367878ac31cd1
StefanoWind <ltzsfn@gmail.com>
2025-07-10 16:26:19 -0600

    created and finalized m5_00_a1

---
+++ NWTC_test/m5_00_a1.py
@@ -0,0 +1,154 @@
+'''
+Format raw M5 data into a1 netCDF
+'''
+
+import os
+cd=os.path.dirname(__file__)
+import scipy.io as spio
+import matplotlib.pyplot as plt
+import paramiko
+from scp import SCPClient
+import glob
+import xarray as xr
+import yaml
+import numpy as np
+from datetime import datetime, timedelta
+import os
+import warnings
+plt.close('all')
+warnings.filterwarnings("ignore")
+
+#%% Inputs
+path_config=os.path.join(cd,'configs/config.yaml')
+source='Y:/Wind-data/Public/Projects/Met135/MetData/M5Twr'
+sdate='2022-07-26'#[%Y-%m-%d] start date
+edate='2022-07-26'#[%Y-%m-%d] end date
+storage=os.path.join(cd,'data/nwtc/nwtc.m5.a0')#where to save
+destination='/scratch/sletizia/ASSIST_analysis/NWTC_test/data/nwtc/nwtc.m5.a0'#storage location on Kestrel
+replace=False#replace existing files?
+send=True#send files to server?
+delete=True#delete local files?
+
+zero_datenum=719529#[days] 1970-01-01 in matlab time
+
+#variables
+variables={'air_temp':'Air_Temp_{z}m',
+           'dewp_temp':'Dewpt_Temp_{z}m',
+           'press':'Baro_Presr_{z}m',
+           'precip':'Precip_TF',
+           'ws':'Cup_WS_{z}m',
+           'wd':'Vane_WD_{z}m'}
+
+#units (in mat structures)
+units={'air_temp':'C',
+       'dewp_temp':'C',
+       'press':'kPa',
+       'precip':'unitless',
+       'ws':'m/s',
+       'wd':'degrees'}
+
+#sensor heights a.g.l.
+heights=[3,38,87,122]
+
+oversample=20
+       
+
+#%% Functions
+def extract_data(day,source,storage):
+    files=sorted(glob.glob(os.path.join(source,day.strftime('%Y/%m/%d')+'/raw_data/*.mat')))
+    
+    for f in files:
+        
+        #comopse filename
+        time_str = datetime.strptime(os.path.basename(f)[:-4], "%m_%d_%Y_%H_%M_%S_%f").strftime("%Y%m%d.%H%M%S")
+        filename=f'nwtc.m5.a1.{time_str}.nc'
+        
+        if replace==False and os.path.exists(os.path.join(storage,filename))==True:
+            print(f'{f} skipped')
+        else:
+            data=xr.Dataset()
+            
+            #read mat structure
+            mat = spio.loadmat(f, struct_as_record=False, squeeze_me=True)
+            
+            #matlab datenum to numpy
+            tnum=(mat['time_UTC'].val[::oversample]-zero_datenum)*24*3600
+            time=np.datetime64('1970-01-01T00:00:00')+np.timedelta64(1,'ms')*np.int64(tnum*1000)
+    
+            #ingest variable
+            for v in variables:
+                var=np.zeros((len(time),len(heights)))+np.nan
+                i=0
+                for h in heights:
+                    try:
+                        var[:,i]=mat[variables[v].format(z=h)].val[::oversample]
+                    except:
+                        pass
+                    i+=1
+                
+                data[v]=xr.DataArray(data=var,coords={'time':time,'height':heights},attrs={'units':units[v]})
+                                       
+            #reconstruct air temperature from delta T
+            T_rec=np.zeros((len(time),len(heights)))+np.nan
+            T_rec[:,0]=mat['Air_Temp_{z}m'.format(z=heights[0])].val[::oversample]
+            for i in range(len(heights)-1):
+                h1=heights[i]
+                h2=heights[i+1]
+                T_rec[:,i+1]=T_rec[:,i]+mat['DeltaT_{z2}_{z1}m'.format(z2=h2,z1=h1)].val[::oversample]
+            
+            data['air_temp_rec']=xr.DataArray(data=T_rec,coords={'time':time,'height':heights},attrs={'units':units[v]})
+            
+            #output
+            data=data.sortby('time').to_netcdf(os.path.join(storage,filename))
+            print(f'{filename} created')
+            
+            #list remote files
+            if send:
+                stdin, stdout, stderr = ssh.exec_command(f'ls {destination}')
+                transfered=np.array(stdout.read().decode().split('\n'))
+    
+                files=glob.glob(source)
+    
+                #transfer
+                with SCPClient(ssh.get_transport()) as scp:
+                   
+                    if np.sum(filename==transfered)==0:
+                        scp.put(os.path.join(storage,filename), remote_path=destination)  
+                        print(f"{filename} sent")
+                        if delete:
+                            os.remove(os.path.join(storage,filename))
+                            print(f"{filename} deleted locally")
+                    else:
+                        print(f"{filename} skipped")
+
+            
+#%% Initialization
+with open(path_config, 'r') as fid:
+    config = yaml.safe_load(fid)
+    
+os.makedirs(storage,exist_ok=True)
+
+start = datetime.strptime(sdate, '%Y-%m-%d')
+end = datetime.strptime(edate, '%Y-%m-%d')
+days = [start + timedelta(days=i) for i in range((end - start).days + 1)]
+
+#connect to Kestrel
+if send:
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(config['host'], username=config['username'], password=config['password'])
+    ssh.exec_command(f'mkdir -p {destination}')
+
+#%% Main   
+args = [(days[i],source,storage,config,send,delete,destination) for i in range(len(days))]
+
+for d in days:
+    extract_data(d, source,storage)
+
+if send:
+    ssh.close()
+            
+
+
+            
+        
--- NWTC_test/profiles_stability.py
+++ NWTC_test/profiles_stability.py
@@ -18,6 +18,9 @@ matplotlib.rcParams['font.size'] = 14
 
 #%% Inputs
 
+#user
+criterion='Ri'
+
 #dataset
 source_stab=os.path.join(cd,'data/nwtc/nwtc.m5.c0/*nc')#source of met stats
 source_waked=os.path.join(cd,'data/turbine_wakes.nc')
@@ -36,12 +39,20 @@ var_met='temperature'#selected temperature variable in M5 data
 
 #stats
 stab_classes_uni=['S','NS','N','NU','U']
-stab_classes={'S':[0,200],
-              'NS':[200,500],
-              'N1':[500,np.inf],
-              'N2':[-np.inf,-500],
-              'NU':[-500,-200],
-              'U':[-200,0]}#stability classes from Obukhov length [Hamilton and Debnath, 2019]
+if criterion=='L':
+    stab_classes={'S':[0,200],
+                  'NS':[200,500],
+                  'N1':[500,np.inf],
+                  'N2':[-np.inf,-500],
+                  'NU':[-500,-200],
+                  'U':[-200,0]}#stability classes from Obukhov length [Hamilton and Debnath, 2019]
+elif criterion=='Ri':
+    stab_classes={'S':[2,10],
+                  'NS':[0.2,2],
+                  'N':[-0.2,0.2],
+                  'NU':[-2,-0.2],
+                  'U':[-10,-2]}#stability classes from Richardson number [Hamilton and Debnath, 2019]
+
 p_value=0.05
 
 #graphics
@@ -63,6 +74,7 @@ Data_met=Data_met.where(Data_met.time_diff<=max_time_diff)
 files=glob.glob(source_stab)
 met=xr.open_mfdataset(files)
 L=met.L.mean(dim="height_kin").interp(time=Data_trp.time)
+Ri=met.Ri.interp(time=Data_trp.time)
 
 #hour
 hour=[(t-np.datetime64(str(t)[:10]))/np.timedelta64(1,'h') for t in Data_met.time.values]
@@ -80,10 +92,14 @@ colors=[cmap(val) for val in np.linspace(0, 1, len(stab_classes_uni))]
 stab_class=xr.DataArray(data=['null']*len(L.time),coords={'time':L.time})
 
 for s in stab_classes.keys():
-    sel=(L>=stab_classes[s][0])*(L<stab_classes[s][1])
-    if s=='N1' or s=='N2':
-        s='N'
-    stab_class=stab_class.where(~sel,other=s)
+    if criterion=='L':
+        sel=(L>=stab_classes[s][0])*(L<stab_classes[s][1])
+        if s=='N1' or s=='N2':
+            s='N'
+        stab_class=stab_class.where(~sel,other=s)
+    elif criterion=='Ri':
+        sel=(Ri>=stab_classes[s][0])*(Ri<stab_classes[s][1])
+        stab_class=stab_class.where(~sel,other=s)
     
 #remove wakes
 Data_trp['waked']=waked['Site 3.2'].interp(time=Data_trp.time)
@@ -104,76 +120,38 @@ real=np.isnan(f_trp).sum(dim="height")+np.isnan(f_met).sum(dim="height")==0
 f_trp=f_trp[real]
 f_met=f_met[real]
 
-# #stats
-# f_trp_avg=np.zeros((len(f_trp.height),len(stab_classes_uni)))
-# f_trp_low=np.zeros((len(f_trp.height),len(stab_classes_uni)))
-# f_trp_top=np.zeros((len(f_trp.height),len(stab_classes_uni)))
-# for i_sc in range(len(stab_classes_uni)):
-#     sc=stab_classes_uni[i_sc]
-#     for i_h in range(len(f_trp.height)):
-#         f_sel=f_trp.isel(height=i_h).where(stab_class==sc).values
-#         f_trp_avg[i_h,i_sc]=utl.filt_stat(f_sel,np.nanmedian)
-#         f_trp_low[i_h,i_sc]=utl.filt_BS_stat(f_sel,np.nanmedian,p_value/2*100)
-#         f_trp_top[i_h,i_sc]=utl.filt_BS_stat(f_sel,np.nanmedian,(1-p_value/2)*100)
-
-# trp_stats=xr.Dataset()
-# trp_stats['f_avg']=xr.DataArray(data=f_trp_avg,coords={'height':f_trp.height,'_class':stab_classes_uni})
-# trp_stats['f_low']=xr.DataArray(data=f_trp_low,coords={'height':f_trp.height,'_class':stab_classes_uni})
-# trp_stats['f_top']=xr.DataArray(data=f_trp_top,coords={'height':f_trp.height,'_class':stab_classes_uni})
-
-# f_met_avg=np.zeros((len(f_met.height),len(stab_classes_uni)))
-# f_met_low=np.zeros((len(f_met.height),len(stab_classes_uni)))
-# f_met_top=np.zeros((len(f_met.height),len(stab_classes_uni)))
-# for i_sc in range(len(stab_classes_uni)):
-#     sc=stab_classes_uni[i_sc]
-#     for i_h in range(len(f_met.height)):
-#         f_sel=f_met.isel(height=i_h).where(stab_class==sc).values
-#         f_met_avg[i_h,i_sc]=utl.filt_stat(f_sel,np.nanmean)
-#         f_met_low[i_h,i_sc]=utl.filt_BS_stat(f_sel,np.nanmean,p_value/2*100)
-#         f_met_top[i_h,i_sc]=utl.filt_BS_stat(f_sel,np.nanmean,(1-p_value/2)*100)
-
-# met_stats=xr.Dataset()
-# met_stats['f_avg']=xr.DataArray(data=f_met_avg,coords={'height':f_met.height,'_class':stab_classes_uni})
-# met_stats['f_low']=xr.DataArray(data=f_met_low,coords={'height':f_met.height,'_class':stab_classes_uni})
-# met_stats['f_top']=xr.DataArray(data=f_met_top,coords={'height':f_met.height,'_class':stab_classes_uni})
-
-
 #stats
-bins_hour=np.arange(0,25,4)
-hour_avg=(bins_hour[:-1]+bins_hour[1:])/2
-f_trp_avg=np.zeros((len(f_trp.height),len(hour_avg)))
-f_trp_low=np.zeros((len(f_trp.height),len(hour_avg)))
-f_trp_top=np.zeros((len(f_trp.height),len(hour_avg)))
-for i_h in range(len(bins_hour)-1):
-    h1=bins_hour[i_h]
-    h2=bins_hour[i_h+1]
-    for i_z in range(len(f_trp.height)):
-        f_sel=f_trp.isel(height=i_z).where(Data_met.hour>=h1).where(Data_met.hour<h2).values
-        f_trp_avg[i_z,i_h]=utl.filt_stat(f_sel,np.nanmedian)
-        f_trp_low[i_z,i_h]=utl.filt_BS_stat(f_sel,np.nanmedian,p_value/2*100)
-        f_trp_top[i_z,i_h]=utl.filt_BS_stat(f_sel,np.nanmedian,(1-p_value/2)*100)
+f_trp_avg=np.zeros((len(f_trp.height),len(stab_classes_uni)))
+f_trp_low=np.zeros((len(f_trp.height),len(stab_classes_uni)))
+f_trp_top=np.zeros((len(f_trp.height),len(stab_classes_uni)))
+for i_sc in range(len(stab_classes_uni)):
+    sc=stab_classes_uni[i_sc]
+    for i_h in range(len(f_trp.height)):
+        f_sel=f_trp.isel(height=i_h).where(stab_class==sc).values
+        f_trp_avg[i_h,i_sc]=utl.filt_stat(f_sel,np.nanmedian)
+        f_trp_low[i_h,i_sc]=utl.filt_BS_stat(f_sel,np.nanmedian,p_value/2*100)
+        f_trp_top[i_h,i_sc]=utl.filt_BS_stat(f_sel,np.nanmedian,(1-p_value/2)*100)
 
 trp_stats=xr.Dataset()
-trp_stats['f_avg']=xr.DataArray(data=f_trp_avg,coords={'height':f_trp.height,'hour':hour_avg})
-trp_stats['f_low']=xr.DataArray(data=f_trp_low,coords={'height':f_trp.height,'hour':hour_avg})
-trp_stats['f_top']=xr.DataArray(data=f_trp_top,coords={'height':f_trp.height,'hour':hour_avg})
+trp_stats['f_avg']=xr.DataArray(data=f_trp_avg,coords={'height':f_trp.height,'_class':stab_classes_uni})
+trp_stats['f_low']=xr.DataArray(data=f_trp_low,coords={'height':f_trp.height,'_class':stab_classes_uni})
+trp_stats['f_top']=xr.DataArray(data=f_trp_top,coords={'height':f_trp.height,'_class':stab_classes_uni})
 
-f_met_avg=np.zeros((len(f_met.height),len(hour_avg)))
-f_met_low=np.zeros((len(f_met.height),len(hour_avg)))
-f_met_top=np.zeros((len(f_met.height),len(hour_avg)))
-for i_h in range(len(bins_hour)-1):
-    h1=bins_hour[i_h]
-    h2=bins_hour[i_h+1]
-    for i_z in range(len(f_met.height)):
-        f_sel=f_met.isel(height=i_z).where(Data_met.hour>=h1).where(Data_met.hour<h2).values
-        f_met_avg[i_z,i_h]=utl.filt_stat(f_sel,np.nanmedian)
-        f_met_low[i_z,i_h]=utl.filt_BS_stat(f_sel,np.nanmedian,p_value/2*100)
-        f_met_top[i_z,i_h]=utl.filt_BS_stat(f_sel,np.nanmedian,(1-p_value/2)*100)
+f_met_avg=np.zeros((len(f_met.height),len(stab_classes_uni)))
+f_met_low=np.zeros((len(f_met.height),len(stab_classes_uni)))
+f_met_top=np.zeros((len(f_met.height),len(stab_classes_uni)))
+for i_sc in range(len(stab_classes_uni)):
+    sc=stab_classes_uni[i_sc]
+    for i_h in range(len(f_met.height)):
+        f_sel=f_met.isel(height=i_h).where(stab_class==sc).values
+        f_met_avg[i_h,i_sc]=utl.filt_stat(f_sel,np.nanmean)
+        f_met_low[i_h,i_sc]=utl.filt_BS_stat(f_sel,np.nanmean,p_value/2*100)
+        f_met_top[i_h,i_sc]=utl.filt_BS_stat(f_sel,np.nanmean,(1-p_value/2)*100)
 
 met_stats=xr.Dataset()
-met_stats['f_avg']=xr.DataArray(data=f_met_avg,coords={'height':f_met.height,'hour':hour_avg})
-met_stats['f_low']=xr.DataArray(data=f_met_low,coords={'height':f_met.height,'hour':hour_avg})
-met_stats['f_top']=xr.DataArray(data=f_met_top,coords={'height':f_met.height,'hour':hour_avg})
+met_stats['f_avg']=xr.DataArray(data=f_met_avg,coords={'height':f_met.height,'_class':stab_classes_uni})
+met_stats['f_low']=xr.DataArray(data=f_met_low,coords={'height':f_met.height,'_class':stab_classes_uni})
+met_stats['f_top']=xr.DataArray(data=f_met_top,coords={'height':f_met.height,'_class':stab_classes_uni})
 
 #%% Plots
 
@@ -201,31 +179,4 @@ for i_sc in range(len(stab_classes_uni)):
     plt.title(stab_classes_uni[i_sc])
 plt.tight_layout()
 plt.legend()
-    
-
-#average profiles
-plt.figure(figsize=(18,4))
-
-for i_h in range(len(hour_avg)):
-    plt.subplot(1,len(hour_avg),i_h+1)
-    plt.plot(met_stats.f_avg.isel(hour=i_h),met_stats.height,'.-k',label='Met')
-    plt.fill_betweenx(met_stats.height,met_stats.f_low.isel(hour=i_h),
-                                      met_stats.f_top.isel(hour=i_h),
-                                      color='k',alpha=0.25)
-    plt.plot(trp_stats.f_avg.isel(hour=i_h),trp_stats.height,'.-r',label='TROPoe')
-    plt.fill_betweenx(trp_stats.height,trp_stats.f_low.isel(hour=i_h),
-                                      trp_stats.f_top.isel(hour=i_h),
-                                      color='r',alpha=0.25)
-    plt.plot(-g/cp*f_trp.height+met_stats.f_avg.isel(hour=i_h).isel(height=0),trp_stats.height,'--k')
-    
-    plt.xlim([15,30])
-    plt.grid()
-    plt.xlabel(r'$T$ [$^\circ$C]')
-    if i_sc==0:
-        plt.ylabel(r'$z$ [m]')
-    
-    plt.title(hour_avg[i_h])
-plt.tight_layout()
-plt.legend()
-    
     
